f4f8d6596a3bef03ba5d8724b03a14e48d6ce100
Trevor Meyer <trvrmeyer@gmail.com>
2015-03-24 08:21:33 -0600

    single database credentials instead of localhost included

--- cellSpan/phoneDBConnector.php
+++ cellSpan/phoneDBConnector.php
@@ -3,28 +3,14 @@
 function loadDatabase()
 {
    // strictly for online phone client -> db access
-   $dbHost = "localhost";
-   $dbPort = "";
-   $dbUser = "root";
-   $dbPassword = "kumite2";
-   $dbName = "nex";
+   $dbHost = "127.3.95.130";
+   $dbPort = "3306";
+   $dbUser = "phoneAccess";
+   $dbPassword = "bqD54J3Fe6b5ncDr";
+   $dbName = "java313";
 
-   $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
-     if ($openShiftVar === null || $openShiftVar == "")
-     {
-          // Not in the openshift environment
-          // echo "Using local credentials: "; 
-     }
-     else 
-     { 
-         $dbHost = "ex-std-node518.prod.rhcloud.com";
-         $dbPort = "3306";
-         $dbUser = "phoneAccess";
-         $dbPassword = "rH6Gmm94uwAaNEqd";
-         $dbName = "java313";
-     }
-   echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+   // echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
 
    return $db;
