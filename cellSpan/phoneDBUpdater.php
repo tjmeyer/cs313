b7ee9bf48aaddbfe1c17b5f441c5400abd401274
Trevor Meyer <trvrmeyer@gmail.com>
2015-03-24 04:49:52 -0600

    phone login functionality initial add

--- cellSpan/phoneDBUpdater.php
+++ cellSpan/phoneDBUpdater.php
@@ -1,17 +1,41 @@
 <?php
 
-function loadDatabase()
+include("phoneDBConnector.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-   // strictly for online phone client -> db access
-   $dbHost = "127.3.95.130";
-   $dbPort = "3306";
-   $dbUser = "phoneAccess";
-   $dbPassword = "phonepassword";
-   $dbName = "java313";
-
-   // echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
-   $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
-
-   return $db;
+   $db = loadDatabase();
+   $lat = $_POST['lat'];
+   $lon = $_POST['lon'];
+   $time = $_POST['time'];
+   $uuid = $_POST['uuid'];
+   
+   if($lat && $lon && $time && $uuid != "null") //if none are null or invalid
+   {
+      try {
+         //pull phone from db
+         $query = "SELECT id FROM phone WHERE mac = \"".$uuid."\"";
+         $statement = $db->query($query);
+         $phone = $statement->fetch(PDO::FETCH_ASSOC);
+      } catch (Exception $e) {
+         die("SELECT ERROR: ".$e);
+      }
+      
+      try {
+         //insert new location
+         $query = "INSERT INTO location(latitude, longitude, phone_id, time_stamp) VALUES (:lat, :lon, :phone_id, :time)";
+         $timestamp = $time/1000;
+         $date = date("Y-m-d", $timestamp);
+         $statement = $db->prepare($query);
+         $statement->bindParam(":lat", $lat);
+         $statement->bindParam(":lon", $lon);
+         $statement->bindParam(":phone_id", $phone['id']);
+         $statement->bindParam(":time", $date);
+         $statement->execute();
+      } catch (Exception $e) {
+         die("INSERT ERROR: ".$e);
+      }
+      
+   }
 }
 ?>
